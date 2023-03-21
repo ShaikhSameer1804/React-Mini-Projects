e7826990fbdcd1eb03c488cd0c545175cfa14779
ShaikhSameer1804 <sameer181617@gmail.com>
2023-03-21 18:06:11 +0530

    Sameer- Added one project

---
+++ emi-calculator/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import EmiCalculator from "./Components/EmiCalculator";
+
+function App() {
+  return (
+    <div className="app">
+      <EmiCalculator />
+    </div>
+  );
+}
+
+export default App;
